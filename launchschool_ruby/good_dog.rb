4597b906d162053889d889631c4ca8ad583d9687
Enze Zhao <zhaoenze001@gmail.com>
2021-03-27 19:51:19 +0000

    good_dog.rb

---
+++ launchschool_ruby/good_dog.rb
@@ -0,0 +1,38 @@
+module Speak
+    def speak(sound)
+        puts sound
+    end
+end
+
+module Eat
+    def eat(food)
+        puts food
+    end
+end
+
+class GoodDog
+    include Speak
+end
+
+class HumanBeing
+    include Speak
+end
+
+class Pugs
+    include Eat
+end
+#pugs like to eat bamboo!
+greypug=Pugs.new
+greypug.eat("Bamboo")
+
+sparky = GoodDog.new
+sparky.speak("Arf")
+bob=HumanBeing.new
+bob.speak("Hello")
+
+
+puts "--gooddog ancestors--"
+puts GoodDog.ancestors
+puts ''
+puts "--HumanBeing ancestors---"
+puts HumanBeing.ancestors
