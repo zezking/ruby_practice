5073c748bc7ce2b01a9cd6e8d796b83eccd4a4e4
Enze Zhao <zhaoenze001@gmail.com>
2021-03-27 22:03:02 +0000

    add change color methods

--- launchschool_ruby/02_exercise.rb
+++ launchschool_ruby/02_exercise.rb
@@ -8,32 +8,48 @@ attr_accessor :year, :color,:model
     end
 
     def speed
-        speed=0
+        "The current speed is #{@speed}"
     end
 
     def speedup
-        @speed+=50
+        @speed>50 ? "you gotta slow down" : @speed+=30
     end
 
     def brake
-        if @speed==0
-            puts "you gotta get the car running stoopid"
-        else
-            @speed-=30
-        end
+        @speed==0 ? "you gotta start the car stoopid" : @speed-=30
+    end
+
+    def shutdown
+        @speed==0 ? "It's already not going anywhere stoooopid" : @speed=0
+    end
+    def change_color(c)
+        self.color=c
+    end
+
+    def spray_paint
+        puts "Tell me what color you want to paint the car my man:  "
+        self.color=gets.to_s.chomp
     end
 
     def info
-        "Year #{self.year} Color #{self.color} Model #{self.model} Current Speed is #{@speed}"
+        "Year: #{self.year} Color: #{self.color} Model: #{self.model} Current Speed: is #{@speed}"
     end
 end
 
 
-oldcar=MyCar.new("1900", "black", "ford model-T")
+oldcar=MyCar.new("1900", "black", "Ford model-T")
 
+# puts oldcar.info
+# puts oldcar.brake
+# puts oldcar.speedup
+# puts oldcar.speedup
+# puts oldcar.speedup
+# puts oldcar.speed
+# puts oldcar.brake
+# puts oldcar.speed
+# puts oldcar.shutdown
+# puts oldcar.speed
+
+# oldcar.change_color("yellow")
+oldcar.spray_paint
 puts oldcar.info
-puts oldcar.brake
-puts oldcar.speedup
-puts oldcar.speed
-puts oldcar.brake
-puts oldcar.speed
